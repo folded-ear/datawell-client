29be1bc406f9e84cf7f04c7a4451f9778a13661a
Brenna Switzer <brenna.switzer@gmail.com>
2016-05-24 12:30:59 -0700

    Cleaning up vocabulary in prep for screwing around with routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,3 @@
-/*
- * Angular 2 decorators and services
- */
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouteConfig, Router } from '@angular/router-deprecated';
 
@@ -24,8 +21,8 @@ import { RouterActive } from './router-active';
   ],
   template: `
     <span router-active>
-      <button [routerLink]=" ['Index'] ">
-        Index
+      <button [routerLink]=" ['AddEvent'] ">
+        Add Event
       </button>
     </span>
     <span router-active>
@@ -47,7 +44,7 @@ import { RouterActive } from './router-active';
   `
 })
 @RouteConfig([
-  { path: '/',      name: 'Index', component: AddEvent, useAsDefault: true },
+  { path: '/',      name: 'AddEvent', component: AddEvent, useAsDefault: true },
   { path: '/home',  name: 'Home',  component: Home },
   // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
   { path: '/about', name: 'About', loader: () => require('es6-promise!./about')('About') }
@@ -68,11 +65,3 @@ export class App {
   }
 
 }
-
-/*
- * Please review the https://github.com/AngularClass/angular2-examples/ repo for
- * more angular app examples that you may copy/paste
- * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
- * For help or questions please contact us at @AngularClass on twitter
- * or our chat on Slack at https://AngularClass.com/slack-join
- */
