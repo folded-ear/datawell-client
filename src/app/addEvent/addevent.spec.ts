1de8e638b9298fca9a570a5edfce05b5b54cfbf4
Brenna Switzer <brenna.switzer@gmail.com>
2016-05-24 12:07:06 -0700

    Adding my first unit test

---
+++ src/app/addEvent/addevent.spec.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {
+    beforeEachProviders,
+    describe,
+    inject,
+    injectAsync,
+    it
+} from '@angular/core/testing';
+
+describe('AddEvent', () => {
+
+    it('true is true', () => expect(true).toEqual(true));
+
+});
